a8920ac994dd0363d6965093a5108e24ec606029
MoonlightGuardian <wq.work.pro@gmail.com>
2021-11-02 14:13:59 +0800

    uodate

--- aa.sh
+++ aa.sh
@@ -1,7 +1,7 @@
-rm -r docs/ #删除docs文件夹#
+rm -r docs/ #删除docs文件夹   docs文件夹是生成的静态网页文件目录#
 git rm -r --cached docs/ #告诉git我删除了docs文件夹#
 
-
+#下边是上传文件到git#
 git init
 git add .
 git commit -m "uodate"
@@ -9,7 +9,7 @@ git branch -M main
 git remote add origin https://github.com/MoonlightGuardian/fengfan.github.io.git
 git push -u origin main
 
-
+# 是生成docs网页
 hugo --destination ./docs --buildDrafts --cleanDestinationDir   
 
 git init
@@ -18,6 +18,6 @@ git commit -m "uodate"
 git branch -M main
 git remote add origin https://github.com/MoonlightGuardian/fengfan.github.io.git
 git push -u origin main
-
+# 运行本地网站  -D 是显示草稿
 hugo server  -D
 
