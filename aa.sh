c98713172b6c8a99a5ac2ab0c9e94cde8d4d5e3d
MoonlightGuardian <wq.work.pro@gmail.com>
2021-11-02 13:50:49 +0800

    uodate

--- aa.sh
+++ aa.sh
@@ -1,3 +1,13 @@
+git rm -r --cached docs/
+rm -r docs/
+git init
+git add .
+git commit -m "uodate"
+git branch -M main
+git remote add origin https://github.com/MoonlightGuardian/fengfan.github.io.git
+git push -u origin main
+
+
 hugo --destination ./docs --buildDrafts --cleanDestinationDir   
 
 git init
