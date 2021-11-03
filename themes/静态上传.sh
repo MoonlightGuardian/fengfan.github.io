1fbf95516517f13022b257d9f324c7778578ac89
MoonlightGuardian <wq.work.pro@gmail.com>
2021-11-03 22:45:27 +0800

    uodate

--- themes/静态上传.sh
+++ themes/静态上传.sh
@@ -8,11 +8,17 @@ git push -u origin main
 git pull --rebase origin master
 git pull origin main  --allow-unrelated-histories
 
+cd -
+
+hugo --destination ./docs --buildDrafts --cleanDestinationDir 
+
 git init
 git add .
 git commit -m "uodate"
 git branch -M main
-git remote add origin http
+git remote add origin https://github.com/MoonlightGuardian/blog.git
+git push -u origin main
+
 
 rm -rf ./.git
 rm -rf ./静态上传.sh
