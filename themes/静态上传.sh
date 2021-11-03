13b49dc61649c90232959f76b25316829c63ab23
MoonlightGuardian <wq.work.pro@gmail.com>
2021-11-03 22:54:16 +0800

    uodate

--- themes/静态上传.sh
+++ themes/静态上传.sh
@@ -4,18 +4,14 @@ git commit -m "uodate"
 git branch -M main
 git remote add origin https://github.com/MoonlightGuardian/blog.git
 git push -u origin main
-# 下边是拉取解决报错
+
 git pull --rebase origin master
 git pull origin main  --allow-unrelated-histories
 
 cd -
 
 hugo --destination ./docs --buildDrafts --cleanDestinationDir 
-<<<<<<< HEAD
 cd docs
-=======
-
->>>>>>> 243497879758b9588d708611b5eddcb8c80021ba
 git init
 git add .
 git commit -m "uodate"
