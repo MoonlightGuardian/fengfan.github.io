1fa776293db0e20c8c6ff31d76c4de5673cd8e55
MoonlightGuardian <wq.work.pro@gmail.com>
2021-11-03 23:19:35 +0800

    uodate

---
+++ themes/静态上传.sh
@@ -0,0 +1,13 @@
+git init
+git add .
+git commit -m "uodate"
+git branch -M main
+git remote add origin https://github.com/MoonlightGuardian/blog.git
+git push -u origin main
+
+git pull --rebase origin master
+git pull origin main  --allow-unrelated-histories
+
+
+
+
